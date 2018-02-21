3f6e4d80f3806ff9a8d95a7e8006a66c1e70ccd6
RodionovDenis <denisrodionov18@gmail.com>
2018-02-21 12:17:20 +0300

    Change task5

--- template/task1/main.cpp
+++ template/task1/main.cpp
@@ -6,35 +6,35 @@ using namespace std;
 class TWeighingScales
 {
 private:
-	double kg; // вес в киллограммах
+	double kg; // weight in kg
 public:
-	TWeighingScales(double _kg = 0)// конструктор
+	TWeighingScales(double _kg = 0)// constructor
 	{
 		kg = _kg;
 	}
-	TWeighingScales & operator=(const TWeighingScales & kg1) //оператор присваивания
+	TWeighingScales & operator=(const TWeighingScales & kg1) 
 	{
 		kg = kg1.kg;
 		return *this;
 	}
-	void display() //вывод на экран
+	void display() // output on display
 	{
 		cout << "Вес в киллограммах: " << kg << endl;
 	}
-	double killograms() //содержит вес в киллограммах
+	double killograms() 
 	{
 		return kg;
 	}
-	double pharmacyPound() //преобразование в аптечный фунд 
+	double pharmacyPound() //conversion to the phatmscy pound 
 	{
 		return kg * 2.2679;
 	}
 
-	double troyOunce() // преобразование в тройскую унцию 
+	double troyOunce() // conversion to the troy ounce
 	{
 		return kg * 35.2739;
 	}
-	double pood() // преобразование в пуд 
+	double pood() // conversion to the pood
 	{
 		return kg * 0.061;
 	}
@@ -58,35 +58,35 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	double flag;
 	int a, b;
-metka2:cout << "Введите вес в киллограммах:";
+metka2:cout << "Enter the weight in kilograms:";
 	cin >> flag;
 	TWeighingScales weight1, weight2(flag);
 	weight1.operator=(weight2);
-metka3:cout << "\n Выберите действие:\n" << endl;
-	cout << "1. Узнать текущий вес в килограммах\n" << endl;
-	cout << "2. Узнать вес в выбранной единице измерения\n" << endl;
-	cout << "3. Изменить вес в килограммах\n" << endl;
+metka3:cout << "\n Select an actoin:\n" << endl;
+	cout << "1. Learn the current weight in kilograms\n" << endl;
+	cout << "2. Learn the weight of the selected unit\n" << endl;
+	cout << "3. Change the weight in kilograms\n" << endl;
 	cin >> a;
 	switch (a)
 	{
 	case 1: weight2.display(); break;
 	case 2:
 	{
-	metka1:cout << "В какой единице измерения вы предпочитаете узнать вес?\n" << endl;
-		cout << "1. Аптечный фунд" << endl;
-		cout << "2. Тройская унция" << endl;
-		cout << "3. Пуд" << endl;
+	metka1:cout << "In which unit of measurement do you prefer to know the weight?\n" << endl;
+		cout << "1. Pharmacy fund" << endl;
+		cout << "2. Troy ounce" << endl;
+		cout << "3. Pood" << endl;
 		cin >> b;
 		switch (b)
 		{
 		case 1: weight2.displayPharmacyPound(); break;
 		case 2: weight2.displayTroyOunce(); break;
 		case 3: weight2.displayPood(); break;
-		default: cout << "Некорректный ввод. Повторите попытку.\n"; goto metka1;
+		default: cout << "Incorrect entry. Please try again.\n"; goto metka1;
 		}
 		break;
 	}
 	case 3: goto metka2;
-	default: cout << "Некорректный ввод. Повторите попытку.\n"; goto metka3;
+	default: cout << "Incorrect entry. Please try again.\n"; goto metka3;
 	}
 }
